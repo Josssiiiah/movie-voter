e12c5d3237d884b20afd7548844189230bc4127c
Josiah Griggs <josiah@Josiahs-MacBook-Pro-2.local>
2023-09-14 15:25:15 -0500

    Set up Shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full text-center items-center justify-center font-mono text-xl lg:flex">
+      <div className="z-10 max-w-5xl w-full text-center items-center justify-center text-6xl md:text-7xl lg:flex">
         Decide What to Watch <br /> with Friends
       </div>
     </main>
   );
+}
+
+{
+  /* <span className="bg-gradient-to-r from-blue-500 to-slate-500 bg-clip-text text-center text-transparent">
+</span> */
 }
--- components/header.tsx
+++ components/header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="bg-white py-10">
+    <header className= "py-10">
       <nav className="container flex items-center text-sm font-medium tracking-wider uppercase text-stone-500">
         {/* <ul className="flex justify-center gap-8 items-center">
           <li>
