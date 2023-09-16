73071afa409a9a5d1f865e64806660c2c0f8a279
Josiah Griggs <josiah@Josiahs-MacBook-Pro-2.local>
2023-09-15 20:09:12 -0500

    minor changes

--- app/join/page.tsx
+++ app/join/page.tsx
@@ -10,6 +10,7 @@ import {
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { Separator } from "@/components/ui/separator";
+import { Input } from "@/components/ui/input";
 
 export default async function join() {
   // This page will contain behavior to join a group
@@ -28,12 +29,13 @@ export default async function join() {
           {/* <CardDescription>Card Description</CardDescription> */}
           <Separator className="py-1 w-[175px] " />
         </CardHeader>
-        <CardContent className="text-center">
-          <p>Card Content</p>
+        <CardContent className="flex justify-center text-center items-center">
+          <Input className="w-25" placeholder="Enter group code" />
+          <Button variant="default" className="ml-4">
+            Submit
+          </Button>
         </CardContent>
-        <CardFooter className="text-center justify-center">
-          <p>Card Footer</p>
-        </CardFooter>
+        <CardFooter className="text-center justify-center"></CardFooter>
       </Card>
     </div>
   );
